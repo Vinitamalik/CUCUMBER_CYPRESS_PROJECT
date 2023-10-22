8cbce7997593dac85dd8781ad3ebbcae2d0a08ac
VinitaMalik <vinita.malik@voltatrucks.com>
2023-10-22 02:18:01 +0100

    Commi2

--- cypress/support/step_definitions/Test1.js
+++ cypress/support/step_definitions/Test1.js
@@ -10,17 +10,25 @@ const {
 Given("Navigate to the URL", () => {
   cy.visit("https://the-internet.herokuapp.com/");
   cy.log("user logged-in");
+  cy.wait(500);
 });
 
 Then("Click Challenging DOM", () => {
   cy.wait(500);
-  cy.get('a[href*="/challenging_dom"]').click();
+  cy.xpath('//a[text()="Challenging DOM"]').click();
+
 });
 
 Then("Click on RED button and verify that the id of blue, red and green button changes", () => {
   cy.wait(500);
-  var table = cy.xpath('//div[@class="large-2 columns"]');
-  cy.log("table"+ table);
+ // var inputs = document.getElementsByClassName('button');
+  // for(var i =0; i< inputs.length; i++){
+  //   cy.log("table"+ inputs[i].id);
+  // }
+  // cy.log("table"+ inputs.value);
+  // cy.log("table"+ inputs.length);
+ // const y = x.document.getElementById("$6e533c7c180a");
+  
   cy.xpath('//a[@class="button alert"]').click();
   
 });
